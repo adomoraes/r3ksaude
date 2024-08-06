d66b307e8451ee0a95526f92752a9adb593a5acb
Eduardo Moraes <adomoraes@gmail.com>
2024-08-06 08:33:49 -0300

    feat: Removing polygon bg-hero effects

--- src/pages/OQueE.jsx
+++ src/pages/OQueE.jsx
@@ -7,17 +7,6 @@ export default function OQueE() {
 				<section id='oquee'>
 					<div className='bg-rose-600 min-h-screen'>
 						<div className='bg-[url("./assets/bgHeroMic.png")] 2xl:bg-contain 2xl:min-h-screen md:bg-auto bg-[length:20%] bg-bottom relative isolate px-6 pt-14 lg:px-8 h-full overflow-hidden bg-no-repeat'>
-							<div
-								aria-hidden='true'
-								className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
-								<div
-									style={{
-										clipPath:
-											"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-									}}
-									className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-								/>
-							</div>
 							<div className='mx-auto max-w-6xl py-10 pb-36 2xl:max-w-7xl 2xl:py-14 2xl:pb-40'>
 								<div className='flex flex-col md:flex-row'>
 									<div className='md:basis-3/5 p-4 2xl:basis-2/3'>
@@ -59,17 +48,6 @@ export default function OQueE() {
 										</div>
 									</div>
 								</div>
-							</div>
-							<div
-								aria-hidden='true'
-								className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'>
-								<div
-									style={{
-										clipPath:
-											"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-									}}
-									className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
-								/>
 							</div>
 						</div>
 					</div>
